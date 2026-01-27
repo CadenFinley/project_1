ef9ec65d9ed38edadcd9992efb04fea14748f712
CadenFinley <caden.finley0789@gmail.com>
2026-01-27 13:21:16 -0600

    readfile

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+matrix: list[list[str | int]] = []
+kp_file = "test_cases.kp"
+
+def read_file():
+    global matrix
+    with open(kp_file, "r") as file:
+        for line in file:
+            line = line.strip()
+            if line:
+                parts = line.split(", ")
+                if len(parts) == 2:
+                    matrix.append([int(parts[0]), int(parts[1])])
+                elif len(parts) == 3:
+                    matrix.append([parts[0], int(parts[1]), int(parts[2])])
+                    
+def print_matrix():
+    for row in matrix:
+        print(row)
+        
+
 def main():
-    print("Hello, World!")
+    read_file()
+    print_matrix()
+   
 
 if __name__ == "__main__":
     main()
