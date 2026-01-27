dd26313026e425203d2a7ddfff0327476e2a180f
CadenFinley <caden.finley0789@gmail.com>
2026-01-27 13:49:10 -0600

    baseline

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # caden and trent
+import sys
 
 matrix: list[list[str | int]] = []
-kp_file = "test_cases.kp"
+kp_file = ""
 
 def read_file():
     global matrix
@@ -17,26 +18,51 @@ def read_file():
             else:
                 matrix.append([])
                     
-def print_matrix():
-    for row in matrix:
+def print_matrix(local_matrix: list[list[str | int]]):
+    for row in local_matrix:
         print(row)
         
+def create_local_matrix():
+    local_matrix: list[list[str | int]] = []
+    for row in matrix:
+        line = row.copy()
+        if line:
+            local_matrix.append(line)
+        else:
+            break
+    return local_matrix
+
 def greedy_value():
     time_elapsed = 0
+    weight_limit = 0
+    local_matrix = create_local_matrix()
+    
+    print_matrix(local_matrix)
     print(time_elapsed)
     
 def greedy_weight():
     time_elapsed = 0
+    weight_limit = 0
+    local_matrix = create_local_matrix()
+    
+    print_matrix(local_matrix)
     print(time_elapsed)
     
 def greedy_ratio():
     time_elapsed = 0
+    weight_limit = 0
+    local_matrix = create_local_matrix()
+    
+    print_matrix(local_matrix)
     print(time_elapsed)
         
 
 def main():
+    global kp_file
+    kp_file = sys.argv[1]
     read_file()
-    print_matrix()
+    greedy_value()
+    # print_matrix()
    
 
 if __name__ == "__main__":
