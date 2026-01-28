1c9b64b1fc4d3a3e7a2253d64509e07b9b167db8
CadenFinley <caden.finley0789@gmail.com>
2026-01-27 19:03:05 -0600

    greater or equal to

--- main.py
+++ main.py
@@ -47,7 +47,6 @@ def sort_by_weight(local_matrix: list[list[str | int]]):
 def sort_by_ratio(local_matrix: list[list[str | int]]):
     return sorted(local_matrix, key=lambda x: int(x[2]) / int(x[1]), reverse=True)
 
-
 def greedy_value():
     print("Greedy by value:")
     current_weight = 0
@@ -63,14 +62,11 @@ def greedy_value():
         value = row[2]
 
         # if we have enough space in our knapsack then take the value
-        if current_weight+weight < weight_limit:
+        if current_weight+weight <= weight_limit:
             current_weight += weight
             total_value += value
             items_gathered.append(row[0])
 
-    # print(num_of_items)
-    # print(weight_limit)
-    # print_matrix(local_matrix)
     print(items_gathered)
     print(f"Ending weight: {current_weight}")
     print(f"The total value collected: {total_value}")
@@ -92,15 +88,11 @@ def greedy_weight():
         value = row[2]
 
         # if we have enough space in our knapsack then take the value
-        if current_weight+weight < weight_limit:
+        if current_weight+weight <= weight_limit:
             current_weight += weight
             total_value += value
             items_gathered.append(row[0])
 
-            
-    # print(num_of_items)
-    # print(weight_limit)
-    # print_matrix(local_matrix)
     print(items_gathered)
     print(f"Ending weight: {current_weight}")
     print(f"The total value collected: {total_value}")
@@ -127,9 +119,6 @@ def greedy_ratio():
             total_value += value
             items_gathered.append(row[0])
 
-    # print(num_of_items)
-    # print(weight_limit)
-    # print_matrix(local_matrix)
     print(items_gathered)
     print(f"Ending weight: {current_weight}")
     print(f"The total value collected: {total_value}")
